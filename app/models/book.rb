5c137996bb1b5eec84c7748553f18111e0eb00ed
German Grandson <ganieto1@gmail.com>
2015-12-30 15:45:55 -0500

    add finished and recent scopes to Book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,13 @@
 class Book < ActiveRecord::Base
 
+  # scope :finished, ->{where('finished_on IS NOT NULL')} REFACTORED TO BELOW. IS THE SAME OUTCOME
+  scope :finished, ->{where.not(finished_on: nil)}
+  scope :recent, ->{where('finished_on > ?', 1.days.ago)}
+
+  # def self.recent  ANOTHER WAY TO WRITE THE SCOPE :recent ABOVE
+  #   where('finished_on < ?', 2.days.ago)
+  # end
+
   def finished?
     finished_on.present?
   end
