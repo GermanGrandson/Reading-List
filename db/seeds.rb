b6f716b917a2bd03bcfeb046b91d79769fa00006
German Grandson <ganieto1@gmail.com>
2015-12-30 15:31:23 -0500

    Add Books Scaffold and route

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Book.destroy_all
+
+Book.create!([{
+  title: 'Harry Potter',
+  author: 'JK Rowling',
+  description: 'Bacon ipsum dolor amet filet mignon beef flank bacon meatloaf, tongue tenderloin spare ribs ham hock leberkas venison pork loin shankle porchetta. Strip steak biltong pancetta chuck sirloin, boudin ball tip salami ham hock. Shankle tri-tip shank beef pork belly jowl kevin sirloin pastrami ball tip hamburger pork chop picanha tongue. Frankfurter venison porchetta ground round spare ribs tongue flank, rump tri-tip chicken.',
+  amazon_id: '0053285768',
+  rating: 5,
+  finished_on: 10.days.ago
+},
+{
+  title: 'The Game',
+  author: 'Neil Straus',
+  description: 'Bacon ipsum dolor amet shank short ribs flank drumstick ground round. Brisket corned beef tail capicola pork loin tongue. Hamburger pancetta ham hock, kielbasa turducken picanha pork chop tongue alcatra sirloin jerky fatback landjaeger. Ribeye strip steak andouille, porchetta ground round shankle hamburger. T-bone kielbasa pastrami pork chop.',
+  amazon_id: '0348590B2',
+  rating: 2,
+  finished_on: nil
+  }])
+
+  p "Created #{Book.count} books"
