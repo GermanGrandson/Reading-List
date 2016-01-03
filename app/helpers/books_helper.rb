8d2a346c1cb815a172c922bdfbdc54a49169522a
German Grandson <ganieto1@gmail.com>
2016-01-02 22:01:18 -0500

    Small Changes

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,7 +1,6 @@
 module BooksHelper
 
   def image_from_amazon(amazon_id)
-    image_tag 'http://imags.amazon.com/images/P/#{amazon_id}.01.ZTZZZZZZ.jpg'
+    image_tag 'http://ecx.images-amazon.com/images/I/#{amazon_id}_BO1,204,203,200_.jpg'
   end
-  
 end
