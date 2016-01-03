a013c8533e28a55aeb2abde0cc8211b1197afdd2
German Grandson <ganieto1@gmail.com>
2016-01-03 01:06:38 -0500

    Fixed Issue with Book Pics

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,6 +1,6 @@
 module BooksHelper
 
   def image_from_amazon(amazon_id)
-    image_tag 'http://ecx.images-amazon.com/images/I/#{amazon_id}_BO1,204,203,200_.jpg'
+    image_tag "http://ecx.images-amazon.com/images/I/#{amazon_id}_BO1,204,203,200_.jpg", size: "140x180"
   end
 end
